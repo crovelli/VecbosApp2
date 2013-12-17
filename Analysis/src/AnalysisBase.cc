c972a99493626bfbeb1364134a70bbb1e8c66571
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2013-12-17 20:11:29 +0100

    add vertices and other tracks

--- Analysis/src/AnalysisBase.cc
+++ Analysis/src/AnalysisBase.cc
@@ -3,7 +3,11 @@
 #include "DataFormats/include/EventHeader.hh"
 #include "Analysis/include/AnalysisBase.hh"
 
+#include "DataFormats/include/Vertex.hh"
 #include "DataFormats/include/SuperCluster.hh"
+#include "DataFormats/include/Track.hh"
+
+#include "Tools/include/VertexSelector.hh"
 
 using namespace std;
 using namespace vecbos;
@@ -35,10 +39,18 @@ int AnalysisBase::loadTree(Long64_t entry) {
   // load the Event Header (run, lumi,...)
   loadEventHeader();
 
-  // load the Super Cluster collection
+  // load the collection of *good* primary vertices
+  loadPrimaryVertices();
+
+  // load the Super Cluster collections
   loadSuperClusters();
   loadPFSuperClusters();
 
+  // load the tracks collections
+  // loadGeneralTracks();
+  // loadGsfTracks();
+  // loadGlobalMuonTracks();
+  // loadStandaloneMuonTracks();
 
   return centry;
 }
@@ -48,6 +60,31 @@ void AnalysisBase::loadEventHeader() {
   Event.setEventHeader(eventHeader);
 }
 
+
+void AnalysisBase::loadPrimaryVertices() {
+
+  PrimaryVertices.clear();
+
+  VertexCollection tmpVertices;
+  tmpVertices.clear();  
+  for(int i=0; i<nPV; ++i) {
+
+    Vertex::Point position(PVxPV[i],PVyPV[i],PVzPV[i]);
+    Vertex::Error error(PVErrxPV[i],PVErryPV[i],PVErrzPV[i]);
+
+    Vertex vtx(position,error,chi2PV[i],ndofPV[i]);
+    vtx.setValidity(isValidPV[i]);
+    vtx.setFake(isFakePV[i]);
+    vtx.setSumPt(SumPtPV[i]);
+    vtx.setTracksSize(trackSizePV[i]);
+
+    tmpVertices.push_back(vtx);
+  }
+
+  VertexSelector sel(tmpVertices);
+  PrimaryVertices = sel.goodCollection();
+
+}
 
 void AnalysisBase::loadSuperClusters() {
 
@@ -150,4 +187,207 @@ void AnalysisBase::loadPFSuperClusters() {
 
   }
 
+}
+
+
+void AnalysisBase::loadGeneralTracks() {
+
+  GeneralTracks.clear();
+
+  cout << "nPV = " << nPV << endl;
+
+  Track::Point firstGoodPV(PVxPV[0],PVyPV[0],PVzPV[0]);
+
+  for(int i=0; i<nTrack; ++i) {
+
+    Track::Vector momentum(pxTrack[i],pyTrack[i],pzTrack[i]);
+
+    Track track(trackNormalizedChi2Track[i], firstGoodPV, momentum, chargeTrack[i]);
+
+    Track::Point trackVxt(trackVxTrack[i],trackVyTrack[i],trackVzTrack[i]);
+    track.setVertex(trackVxt);
+    
+    // Track::HitPattern pattern;
+    // pattern.numberOfValidHits = trackValidHitsTrack[i];
+    // pattern.numberOfLostHits = trackLostHitsTrack[i];
+    // pattern.numberOfPixelHits = pixelHitsTrack[i];
+    // pattern.trackerLayersWithMeasurement = trackerLayersWithMeasurementTrack[i];
+    // pattern.expectedInnerLayers = expInnerLayersTrack[i];
+    // pattern.numberOfValidPixelBarrelHits = numberOfValidPixelBarrelHitsTrack[i];
+    // pattern.numberOfValidPixelEndcapHits = numberOfValidPixelEndcapHitsTrack[i];
+    // pattern.numberOfValidStripTIBHits = numberOfValidStripTIBHitsTrack[i];
+    // pattern.numberOfValidStripTIDHits = numberOfValidStripTIDHitsTrack[i];
+    // pattern.numberOfValidStripTOBHits = numberOfValidStripTOBHitsTrack[i];
+    // pattern.numberOfValidStripTECHits = numberOfValidStripTECHitsTrack[i];
+    // pattern.numberOfValidMuonHits = numberOfValidMuonHitsTrack[i];
+    // pattern.qualityMask = qualityMaskTrack[i];
+
+    // track.setHitPattern(pattern);
+    
+    // Track::ImpactParameters ips;
+    // ips.ip3D = impactPar3DTrack[i];
+    // ips.ip3Derr = impactPar3DErrorTrack[i];
+    // ips.ip2D = transvImpactParTrack[i];
+    // ips.ip2Derr = transvImpactParErrorTrack[i];
+    // ips.d0 = d0Track[i];
+    // ips.d0err = d0ErrorTrack[i];
+    // ips.dz = dzTrack[i];
+    // ips.dzerr = dzErrorTrack[i];
+    
+    // track.setImpactParameters(ips);
+
+    // track.setPtError(ptErrorTrack[i]);
+
+    GeneralTracks.push_back(track);
+
+    }
+}
+
+void AnalysisBase::loadGsfTracks() {
+
+  GsfTracks.clear();
+
+  Track::Point firstGoodPV(PVxPV[0],PVyPV[0],PVzPV[0]);
+
+  for(int i=0; i<nGsfTrack; ++i) {
+
+    Track::Vector momentum(pxGsfTrack[i],pyGsfTrack[i],pzGsfTrack[i]);
+
+    Track track(trackNormalizedChi2GsfTrack[i], firstGoodPV, momentum, chargeGsfTrack[i]);
+
+    Track::Point trackVxt(trackVxGsfTrack[i],trackVyGsfTrack[i],trackVzGsfTrack[i]);
+    track.setVertex(trackVxt);
+    
+    Track::HitPattern pattern;
+    pattern.numberOfValidHits = trackValidHitsGsfTrack[i];
+    pattern.numberOfLostHits = trackLostHitsGsfTrack[i];
+    pattern.numberOfPixelHits = pixelHitsGsfTrack[i];
+    pattern.trackerLayersWithMeasurement = trackerLayersWithMeasurementGsfTrack[i];
+    pattern.expectedInnerLayers = expInnerLayersGsfTrack[i];
+    pattern.numberOfValidPixelBarrelHits = numberOfValidPixelBarrelHitsGsfTrack[i];
+    pattern.numberOfValidPixelEndcapHits = numberOfValidPixelEndcapHitsGsfTrack[i];
+    pattern.numberOfValidStripTIBHits = numberOfValidStripTIBHitsGsfTrack[i];
+    pattern.numberOfValidStripTIDHits = numberOfValidStripTIDHitsGsfTrack[i];
+    pattern.numberOfValidStripTOBHits = numberOfValidStripTOBHitsGsfTrack[i];
+    pattern.numberOfValidStripTECHits = numberOfValidStripTECHitsGsfTrack[i];
+    pattern.numberOfValidMuonHits = numberOfValidMuonHitsGsfTrack[i];
+    pattern.qualityMask = qualityMaskGsfTrack[i];
+
+    track.setHitPattern(pattern);
+    
+    Track::ImpactParameters ips;
+    ips.ip3D = impactPar3DGsfTrack[i];
+    ips.ip3Derr = impactPar3DErrorGsfTrack[i];
+    ips.ip2D = transvImpactParGsfTrack[i];
+    ips.ip2Derr = transvImpactParErrorGsfTrack[i];
+    ips.d0 = d0GsfTrack[i];
+    ips.d0err = d0ErrorGsfTrack[i];
+    ips.dz = dzGsfTrack[i];
+    ips.dzerr = dzErrorGsfTrack[i];
+    
+    track.setImpactParameters(ips);
+
+    track.setPtError(ptErrorGsfTrack[i]);
+
+    GsfTracks.push_back(track);
+
+    }
+}
+
+void AnalysisBase::loadGlobalMuonTracks() {
+
+  GlobalMuonTracks.clear();
+
+  Track::Point firstGoodPV(PVxPV[0],PVyPV[0],PVzPV[0]);
+
+  for(int i=0; i<nGlobalMuonTrack; ++i) {
+
+    Track::Vector momentum(pxGlobalMuonTrack[i],pyGlobalMuonTrack[i],pzGlobalMuonTrack[i]);
+
+    Track track(trackNormalizedChi2GlobalMuonTrack[i], firstGoodPV, momentum, chargeGlobalMuonTrack[i]);
+
+    Track::Point trackVxt(trackVxGlobalMuonTrack[i],trackVyGlobalMuonTrack[i],trackVzGlobalMuonTrack[i]);
+    track.setVertex(trackVxt);
+    
+    Track::HitPattern pattern;
+    pattern.numberOfValidHits = trackValidHitsGlobalMuonTrack[i];
+    pattern.numberOfLostHits = trackLostHitsGlobalMuonTrack[i];
+    pattern.numberOfPixelHits = pixelHitsGlobalMuonTrack[i];
+    pattern.trackerLayersWithMeasurement = trackerLayersWithMeasurementGlobalMuonTrack[i];
+    pattern.expectedInnerLayers = expInnerLayersGlobalMuonTrack[i];
+    pattern.numberOfValidPixelBarrelHits = numberOfValidPixelBarrelHitsGlobalMuonTrack[i];
+    pattern.numberOfValidPixelEndcapHits = numberOfValidPixelEndcapHitsGlobalMuonTrack[i];
+    pattern.numberOfValidStripTIBHits = numberOfValidStripTIBHitsGlobalMuonTrack[i];
+    pattern.numberOfValidStripTIDHits = numberOfValidStripTIDHitsGlobalMuonTrack[i];
+    pattern.numberOfValidStripTOBHits = numberOfValidStripTOBHitsGlobalMuonTrack[i];
+    pattern.numberOfValidStripTECHits = numberOfValidStripTECHitsGlobalMuonTrack[i];
+    pattern.numberOfValidMuonHits = numberOfValidMuonHitsGlobalMuonTrack[i];
+    pattern.qualityMask = qualityMaskGlobalMuonTrack[i];
+
+    track.setHitPattern(pattern);
+    
+    Track::ImpactParameters ips;
+    ips.ip3D = impactPar3DGlobalMuonTrack[i];
+    ips.ip3Derr = impactPar3DErrorGlobalMuonTrack[i];
+    ips.ip2D = transvImpactParGlobalMuonTrack[i];
+    ips.ip2Derr = transvImpactParErrorGlobalMuonTrack[i];
+    ips.d0 = d0GlobalMuonTrack[i];
+    ips.d0err = d0ErrorGlobalMuonTrack[i];
+    ips.dz = dzGlobalMuonTrack[i];
+    ips.dzerr = dzErrorGlobalMuonTrack[i];
+    
+    track.setImpactParameters(ips);
+
+    track.setPtError(ptErrorGlobalMuonTrack[i]);
+
+    GlobalMuonTracks.push_back(track);
+
+    }
+}
+
+void AnalysisBase::loadStandaloneMuonTracks() {
+
+  StandaloneMuonTracks.clear();
+
+  Track::Point firstGoodPV(PVxPV[0],PVyPV[0],PVzPV[0]);
+
+  for(int i=0; i<nSTAMuonTrack; ++i) {
+
+    Track::Vector momentum(pxSTAMuonTrack[i],pySTAMuonTrack[i],pzSTAMuonTrack[i]);
+
+    Track track(trackNormalizedChi2STAMuonTrack[i], firstGoodPV, momentum, chargeSTAMuonTrack[i]);
+
+    Track::Point trackVxt(trackVxSTAMuonTrack[i],trackVySTAMuonTrack[i],trackVzSTAMuonTrack[i]);
+    track.setVertex(trackVxt);
+    
+    Track::HitPattern pattern;
+    pattern.numberOfValidHits = trackValidHitsSTAMuonTrack[i];
+    pattern.numberOfLostHits = trackLostHitsSTAMuonTrack[i];
+    pattern.numberOfPixelHits = pixelHitsSTAMuonTrack[i];
+    pattern.trackerLayersWithMeasurement = trackerLayersWithMeasurementSTAMuonTrack[i];
+    pattern.expectedInnerLayers = expInnerLayersSTAMuonTrack[i];
+    pattern.numberOfValidPixelBarrelHits = numberOfValidPixelBarrelHitsSTAMuonTrack[i];
+    pattern.numberOfValidPixelEndcapHits = numberOfValidPixelEndcapHitsSTAMuonTrack[i];
+    pattern.numberOfValidStripTIBHits = numberOfValidStripTIBHitsSTAMuonTrack[i];
+    pattern.numberOfValidStripTIDHits = numberOfValidStripTIDHitsSTAMuonTrack[i];
+    pattern.numberOfValidStripTOBHits = numberOfValidStripTOBHitsSTAMuonTrack[i];
+    pattern.numberOfValidStripTECHits = numberOfValidStripTECHitsSTAMuonTrack[i];
+    pattern.numberOfValidMuonHits = numberOfValidMuonHitsSTAMuonTrack[i];
+    pattern.qualityMask = qualityMaskSTAMuonTrack[i];
+
+    track.setHitPattern(pattern);
+    
+    Track::ImpactParameters ips;
+    ips.d0 = d0STAMuonTrack[i];
+    ips.d0err = d0ErrorSTAMuonTrack[i];
+    ips.dz = dzSTAMuonTrack[i];
+    ips.dzerr = dzErrorSTAMuonTrack[i];
+    
+    track.setImpactParameters(ips);
+
+    track.setPtError(ptErrorSTAMuonTrack[i]);
+
+    StandaloneMuonTracks.push_back(track);
+
+    }
 }
